1bc804ae542bb0fefa529285c2fe09987ed4db1e
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-10-22 11:27:53 -0300

    Primer commit: Subiendo código de mi proyecto Magnolia

---
+++ src/Componentes/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../Assets/css/home.css'
+import { IconoHome } from '../Assets/Icons/Icons'
+
+export default function Home() {
+    return (
+        <div><IconoHome /></div>
+    )
+}
