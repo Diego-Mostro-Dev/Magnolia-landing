9666f420e01d77487f8a045a1e27237698a93648
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-10-23 21:40:21 -0300

    Imagenes a funciones y codigo

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import '../Assets/css/home.css'
-import { IconoHome } from '../Assets/Icons/Icons'
+import React from 'react';
+import '../Assets/css/home.css';
+import { IconoHome } from '../Assets/Icons/Icons';
+import {
+    ContenedorRedes,
+    Cruztrabajo,
+    FlechaContacto,
+    LineasTrabajo,
+    Logo,
+    SeccionContacto,
+    ParentesisProductora,
+    ParentesisSomos,
+    SeccionActores,
+    SeccionProductora,
+    SeccionRedes,
+    SeccionSomos,
+    SeccionTrabajo
+} from '../Assets/statics/Imagenes.jsx';
+
+
 
 export default function Home() {
     return (
-        <div><IconoHome /></div>
-    )
+        <>
+            <div><IconoHome /></div>
+            <div><Logo /></div>
+            <div><ContenedorRedes /></div>
+            <div><Cruztrabajo /></div>
+            <div><FlechaContacto /></div>
+            <div><LineasTrabajo /></div>
+            <div><ParentesisProductora /></div>
+            <div><ParentesisSomos /></div>
+            <div><SeccionActores /></div>
+            <div><SeccionContacto /></div>
+            <div><SeccionProductora /></div>
+            <div><SeccionRedes /></div>
+            <div><SeccionSomos /></div>
+            <div><SeccionTrabajo /></div>
+
+        </>
+    );
 }
