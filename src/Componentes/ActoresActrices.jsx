a499fef23323097796baf399ced0d13f0b1bba69
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-10-29 18:35:19 -0300

    css detalles

--- src/Componentes/ActoresActrices.jsx
+++ src/Componentes/ActoresActrices.jsx
@@ -3,7 +3,7 @@ import '../Assets/css/actores-actrices.css'
 export default function ActoresActrices() {
     return (
         <>
-            <div className="actores-actrices">Actores /<br /> Actrices</div>
+            <div className="actores-actrices">Actores/<br />actrices</div>
         </>
     )
 }
