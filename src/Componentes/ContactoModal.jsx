304268762b5a098e1574a4442d8e0474784a5ccf
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-11-06 16:52:43 -0300

    Optimizacion de imagenes y css + carga de slide

--- src/Componentes/ContactoModal.jsx
+++ src/Componentes/ContactoModal.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import '../Assets/css/contacto-modal.css'
 import { useModalContext } from '../ModalContext';
 import { useLanguageContext } from '../LanguageContext';
+import Instagram from '../Assets/statics/instagram.svg';
+import Whatsapp from '../Assets/statics/whatsapp.svg';
+import X from '../Assets/statics/x.svg';
 
 export default function ContactoModal() {
     const { cerrarModal } = useModalContext();
     const { getTraduccion } = useLanguageContext();
     const { titleModal } = getTraduccion('contacto');
     const { text } = getTraduccion('contacto');
+    const { textRedes } = getTraduccion('contacto');
 
     return (
         <div className='contacto-Modal'>
@@ -15,10 +19,14 @@ export default function ContactoModal() {
             <h1>{titleModal}</h1>
             <p>{text}</p>
             <h1>form</h1>
-            <h1>También podés encontrarnos acá:
-                • mail (link)
-                • whatsapp (link)
-                • instagram (link)</h1>
+            <div className="wrapper-contacto-redes">
+                <h1>{textRedes}</h1>
+                <div className="contacto-redes">
+                    <img height={50} width={50} src={Instagram} alt="Instagram" />
+                    <img height={50} width={50} src={Whatsapp} alt="Whatsapp" />
+                    <img height={50} width={50} src={X} alt="X" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/Componentes/TrabajoModal.jsx
+++ src/Componentes/TrabajoModal.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../Assets/css/trabajo-modal.css'
 import { useModalContext } from '../ModalContext';
 import { useLanguageContext } from '../LanguageContext';
+import DSC03595r from '../Assets/statics/FotosMangnolia/DSC03595r.webp';
+import DSC03629r from '../Assets/statics/FotosMangnolia/DSC03629r.webp';
 
 export default function TrabajoModal() {
     const { cerrarModal } = useModalContext();
@@ -13,7 +15,18 @@ export default function TrabajoModal() {
             <button onClick={cerrarModal}>x</button>
             <h1>{titleModal}</h1>
             <p>{text}</p>
-            <div>Carrusel</div>
+            <section>
+                <img height={100} width={50} src={DSC03629r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+                <img height={100} width={50} src={DSC03629r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+                <img height={100} width={50} src={DSC03629r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+                <img height={100} width={50} src={DSC03629r} alt="" />
+                <img height={100} width={50} src={DSC03595r} alt="" />
+            </section>
         </div>
     )
 }
