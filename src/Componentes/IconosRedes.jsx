31fc7416464fb93ff6fef86462f384656f9b04b8
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-12-11 17:42:17 -0300

    mejoras en el css de las paginas internas

--- src/Componentes/IconosRedes.jsx
+++ src/Componentes/IconosRedes.jsx
@@ -10,7 +10,7 @@ export default function IconosRedes() {
             <div className="iconos-redes-redes">
                 <a href="https://www.instagram.com/magnolia_casting" rel="noopener noreferrer" target='_blank'><img height={50} width={50} src={Instagram} alt="Instagram" /></a>
                 <a href="https://wa.me/5493413301861?text=Hola%20Magnolia." rel="noopener noreferrer" target='_blank'><img height={50} width={50} src={Whatsapp} alt="Whatsapp" /></a>
-                <a href="www.gmail.com" target='_blank'><img height={50} width={50} src={correo} alt="correo" /></a>
+                <a href="mailto:info@magnoliacasting.com" rel="noopener noreferrer" target='_blank'><img height={50} width={50} src={correo} alt="correo" /></a>
             </div>
         </>
     )
